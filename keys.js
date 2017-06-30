176e3aa416a2f9c3735143012a19c66920193785
Lexi <you@example.com>
2017-06-29 20:59:13 -0500

    updating dependencies

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'yrvkTnbpbXIDYsHzMymj2AaN9',
+  consumer_secret: 'tGyxoGBCOLH65d0RmpoGwgU8J1Y5ZKrExiLnpoN5LbOQrXaYc0',
+  access_token_key: '879896427048128512-JgI9yN3SpEHWCnvlHuOPUR4TmxTcwE7',
+  access_token_secret: 'jTn8YpVsfAvXpgS5vfACwslGk71jRNG22RrI6VkPX3jwC',
+}
